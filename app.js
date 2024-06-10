8b1c80f777735345108ebc80398e843eedbf7f8d
orwel009 <orwelvincent1@gmail.com>
2024-06-10 16:24:14 +0530

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const monogoos = require("mongoose")
 const cors = require("cors")
+const students = require("./models/student")
 
 const app = express()
 app.use(cors())
@@ -11,6 +12,10 @@ app.get("/",(req,res)=>{
 
 app.post("/contact",(req,res)=>{
     res.send("Welcome to contact page")
+})
+
+app.post("/add",(req,res)=>{
+    res.send("Add Done")
 })
 
 app.listen(8080,()=>{
